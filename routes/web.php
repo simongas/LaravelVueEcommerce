dbc35945f2a27ec26294d2096ee0decea554be6b
Simon Orozco <sorozco@webforcehq.com>
2020-04-04 16:37:04 -0700

    asdf

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/testPass', function(){
-    return Hash::make('simongas0214');
-});
-
-Route::get('/{any}', function () {
-    return view('store');
-})->where('any', '.*');
+Route::get('/{any}', 'HomeController@store')->where('any', '.*');
